8baeb1a6ac3a8c286b1c1aa7fbfd368d8f52d32b
Sammyjones254 <125312252+Sammyjones254@users.noreply.github.com>
2023-02-14 08:01:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+    float a, b, c;
+    float root1, root2;
+    float determinant, denominator;
+    printf("Enter the value of a\n");
+    scanf("%f", &a);
+    printf("Enter the value of b\n");
+    scanf("%f", &b);
+    printf("Enter the value of c\n");
+    scanf("%f", &c);
+    determinant = sqrt (b*b - 4*a*c);
+    denominator = 2*a;
+    root1 = (-b + determinant)/denominator;
+    root2 = (-b - determinant)/denominator;
+    printf("Root 1 = %f\n", root1);
+    printf("Root 2 = %f\n", root2);
+    return 0;
+}
